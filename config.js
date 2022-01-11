55c0c9a5a73f03fbd18c105623427b9d0a92ded3
liam10102 <97449948+liam10102@users.noreply.github.com>
2022-01-11 09:45:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const nameGirl = 'Bé yêu của anh';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Happy Birthday my Love';
+const eventName = 'Happy Birthday';
 const titleCard = 'Tặng bé yêu';
 const contentCard = 'Hôm nay là sinh em, người con gái anh luôn quan tâm và quan trọng nhất trong trái tim anh. Anh chúc em có một ngày sinh nhật tràn ngập niềm vui và hạnh phúc. Anh mong chúng mình sẽ luôn đón sinh nhật cùng với nhau, bây giờ và mãi về sau luôn em nhé ';
 
