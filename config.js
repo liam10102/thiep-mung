73e8c4e37589d24540995cdbdfcbcce35947b371
liam10102 <97449948+liam10102@users.noreply.github.com>
2022-01-11 20:29:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Bé yêu của anh';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Happy Birthday';
 const titleCard = 'Tặng bé yêu';
-const contentCard = 'Hôm nay là sinh em, người con gái anh luôn quan tâm và quan trọng nhất trong trái tim anh. Anh chúc em có một ngày sinh nhật tràn ngập niềm vui và hạnh phúc. Anh mong chúng mình sẽ luôn đón sinh nhật cùng với nhau, bây giờ và mãi về sau luôn em nhé ';
+const contentCard = 'Hôm nay là sinh nhật em, người con gái anh luôn quan tâm và quan trọng nhất trong trái tim anh. Anh chúc em có một ngày sinh nhật tràn ngập niềm vui và hạnh phúc. Anh mong chúng mình sẽ luôn đón sinh nhật cùng với nhau, bây giờ và mãi về sau luôn em nhé!!! ';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
